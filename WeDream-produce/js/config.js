04b24e41faa3dec122b0546257c430cc6d70061b
hhhnl <88999589+hhhnl@users.noreply.github.com>
2025-04-18 13:37:05 +0800

    Update config.js

--- WeDream-produce/js/config.js
+++ WeDream-produce/js/config.js
@@ -1,7 +1,8 @@
 // 创建axios实例
 
 const api = axios.create({
-  baseURL: "http://1.94.9.6:10001",
+  //baseURL: "http://1.94.9.6:10001",
+  baseURL: "https://675b-117-61-109-250.ngrok-free.app/",
 });
 
 
